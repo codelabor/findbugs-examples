b615e37f4f1b7494a8a3e0ee689be5da1c0733ba
bomber.shin <bomber.shin@samsung.com>
2016-01-22 13:33:51 +0900

    comment updated.

--- src/test/java/org/codelabor/example/findbugs/IcastIdivCastToDoubleTest.java
+++ src/test/java/org/codelabor/example/findbugs/IcastIdivCastToDoubleTest.java
@@ -8,6 +8,27 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * <h1>ICAST_IDIV_CAST_TO_DOUBLE</h1>
+ * <p>
+ * This code casts the result of an integral division (e.g., int or long division) operation to double or float. Doing division on integers truncates the result
+ * to the integer value closest to zero. The fact that the result was cast to double suggests that this precision should have been retained. What was probably
+ * meant was to cast one or both of the operands to double before performing the division. Here is an example:
+ * </p>
+ *
+ * <pre>
+ * int x = 2;
+ * int y = 5;
+ * // Wrong: yields result 0.0
+ * double value1 = x / y;
+ *
+ * // Right: yields result 0.4
+ * double value2 = x / (double) y;
+ * </pre>
+ *
+ * @author Shin Sang-jae
+ *
+ */
 public class IcastIdivCastToDoubleTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(IcastIdivCastToDoubleTest.class);
